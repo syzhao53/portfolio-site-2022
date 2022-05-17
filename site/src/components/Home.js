96dab74a9a4e1ae9c294ce1e652ac2a86d51739e
syzhao53 <syzhao@sas.upenn.edu>
2022-05-16 22:02:34 -0400

    initial home commit

--- site/src/components/Home.js
+++ site/src/components/Home.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../assets/index.css';
+import illustration from '../assets/temp-illustration.png';
 
 function Home() {
   // const [error, setError] = useState({ message: '' });
@@ -15,7 +16,20 @@ function Home() {
 
   return (
     <div className="Home">
-      <div>HOME PAGE</div>
+      <div className="landing">
+        <div className="landing-left">
+          <div className="big-header">
+            Hi, I&apos;m Sylvia Zhao
+          </div>
+          <div className="paragraph">
+            I&apos;m a UX and visual designer currently majoring&nbsp;
+            in Cognitive Science and Computer Science at the University of Pennsylvania.
+          </div>
+        </div>
+        <div className="landing-right">
+          <img src={illustration} alt="temp illustration" />
+        </div>
+      </div>
       <button type="button" onClick={goToProject}>Project</button>
     </div>
   );
