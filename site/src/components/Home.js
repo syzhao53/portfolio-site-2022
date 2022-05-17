dc7bc86a310ae17b9e3f22859ec634a33a11dfba
syzhao53 <syzhao@sas.upenn.edu>
2022-05-16 22:27:02 -0400

    NavBar setup and buttons

--- site/src/components/Home.js
+++ site/src/components/Home.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../assets/index.css';
 import illustration from '../assets/temp-illustration.png';
+import NavBar from './NavBar';
 
 function Home() {
   // const [error, setError] = useState({ message: '' });
@@ -16,6 +17,7 @@ function Home() {
 
   return (
     <div className="Home">
+      <NavBar />
       <div className="landing">
         <div className="landing-left">
           <div className="big-header">
@@ -25,12 +27,12 @@ function Home() {
             I&apos;m a UX and visual designer currently majoring&nbsp;
             in Cognitive Science and Computer Science at the University of Pennsylvania.
           </div>
+          <button className="button" type="button" onClick={goToProject}>Project</button>
         </div>
         <div className="landing-right">
           <img src={illustration} alt="temp illustration" />
         </div>
       </div>
-      <button type="button" onClick={goToProject}>Project</button>
     </div>
   );
 }
