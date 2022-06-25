145b3d5fdfd574f28367cccb25b9534ef02615fc
syzhao53 <syzhao@sas.upenn.edu>
2022-06-25 15:43:31 -0400

    interviews

--- site/src/components/PennInTouch.js
+++ site/src/components/PennInTouch.js
@@ -18,7 +18,7 @@ function PennInTouch({ currPage, setCurrPage }) {
 
   console.log(currPage);
 
-  // const footerMessage = 'Made by Sylvia <3';
+  const footerMessage = 'Made by Sylvia <3';
 
   useEffect(() => {
     setCurrPage('');
@@ -30,26 +30,47 @@ function PennInTouch({ currPage, setCurrPage }) {
       <NavBar currPage={currPage} />
       <div className="project-wrap">
         <div className="project-header">PennInTouch</div>
-        <div className="project-info">Redesigning the University of Pennsylvania’s academic planning/registration site, PennInTouch</div>
+        <div className="project-info">Redesigning the University of Pennsylvania&apos;s academic planning/registration site, PennInTouch</div>
         <img className="project-img" src={pennintouch} alt="pennintouch mockup" />
-        <div className="section-header">Overview</div>
-        <div className="project-text">
-          Having used many meeting and event
-          scheduling apps throughout my time as a student, I was curious to see
-          how an experience could be reimagined to be more efficient and
-          usable. I chose to create a site that would address the process of
-          scheduling a group meeting. For the sake of scope, this project only
-          focuses on the side
-          where someone receives an invitation to a meeting and has to provide their availability.
-          <br />
-          <br />
-          This project drew inspiration from the issues of the popular scheduling platform
-          “when2meet.” As found in user research, when2meet provides an convenient method
-          for schedulers to gather group availability, but the platform has poor usability.
-          Let&apos;s Meet attempts to incorporate the positive functionality of when2meet
-          while addressing usability, particularly on mobile.s
+        <div className="section-flex">
+          <div className="section-lflex">
+            <div className="section-header">Overview</div>
+            <div className="project-ltext">
+              The University of Pennsylvania uses PennInTouch as its academic
+              planning and course registration site. Students have constantly
+              complained about the unpleasant experience of using PennInTouch and
+              how it interferes with their planning processes.
+              <br />
+              <br />
+              The purpose of this project was to identify and redesign a key feature
+              PennInTouch. My research process led to a redesign of the Academic
+              Planning Worksheet feature that helps students plan for graduation requirements.
+            </div>
+          </div>
+          <div className="section-rflex">
+            <div className="subsection-rheader-top">
+              ROLE
+            </div>
+            <div className="project-rtext">
+              UX research & design
+            </div>
+            <div className="subsection-rheader">
+              TIMELINE
+            </div>
+            <div className="project-rtext">
+              1.5 months
+            </div>
+          </div>
         </div>
         <div className="section-header"> User Research</div>
+        <div className="cap-card">
+          <div className="cap">
+            <div className="empty">c</div>
+          </div>
+          <div className="cap-card-text">
+            &ldquo;Words cannot describe how terrible the current site is&rdquo; - Penn Student
+          </div>
+        </div>
         <div className="subsection-header">SURVEY</div>
         <div className="project-text">
           I surveyed 16 Penn undergraduate students through Google Forms to collect data on
@@ -100,7 +121,24 @@ function PennInTouch({ currPage, setCurrPage }) {
           </div>
         </div>
       </div>
-      {/* <div className="footer">{footerMessage}</div> */}
+      <div className="project-wrap">
+        <div className="subsection-header-sec">USER INTERVIEWS</div>
+        <div className="project-text">
+          I also conducted user interviews with 3 Penn undergraduate students.
+          The students all cames from different programs across the university and relied on
+          PennInTouch&apos;s planning features in various degrees. From the 12 questions I asked,
+          the general takeaway was that PennInTouch only sufficiently accomplishes its goals
+          and could be improved in various areas.
+          <br />
+          <br />
+          I synthesized recurring comments from the interviews under the categories of
+          academic planning, UI design, general emotions, and navigation issues. Across
+          all interviews, users expressed dissatisfaction with outdated UI design, difficulty
+          understanding the Academic Planning Worksheet&apos;s interactions, and the site&apos;s
+          use of a crowded sidebar for navigation.
+        </div>
+      </div>
+      <div className="footer">{footerMessage}</div>
     </div>
     // <button className="button" type="button" onClick={goToProject}>View my work</button>
     // TODO: ADD FLOATING CARAT ARROW
