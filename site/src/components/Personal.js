38044b610bb93b1aa160ba04ca4917c94e77c4df
syzhao53 <syzhao@sas.upenn.edu>
2022-07-22 15:23:17 -0400

    updated misc. assets

--- site/src/components/Personal.js
+++ site/src/components/Personal.js
@@ -3,6 +3,13 @@
 import React, { useEffect } from 'react';
 // import { useNavigate } from 'react-router-dom';
 import '../assets/index.css';
+import icecream from '../assets/ice-cream.png';
+import staticlanding from '../assets/static-site-landing.png';
+import pennapps from '../assets/pennapps-mockup.png';
+import shea from '../assets/shea-butter.png';
+import reishi from '../assets/reishi-mushroom.png';
+// import burgundylady from '../assets/burgundy-lady.png';
+// import comfyfarm from '../assets/comfy-farm.png';
 import NavBar from './NavBar';
 import Footer from './Footer';
 
@@ -20,8 +27,15 @@ function Personal({ currPage, setCurrPage }) {
       <NavBar currPage={currPage} />
       <div className="project-wrap">
         <div className="project-header">
-          Personal Work
+          Miscellaneous
         </div>
+        <img className="misc-img" src={icecream} alt="ice cream illustration" />
+        <img className="misc-img" src={staticlanding} alt="pennapps static site landing illustration" />
+        <img className="misc-img" src={pennapps} alt="pennapps static site small illustrations" />
+        <img className="misc-img" src={shea} alt="shea butter illustration" />
+        <img className="misc-img" src={reishi} alt="reishi mushroom illustration" />
+        {/* <img className="misc-img" src={comfyfarm} alt="comfy farm illustration" /> */}
+        {/* <img className="misc-img" src={burgundylady} alt="burgundy lady illustration" /> */}
       </div>
       <Footer />
     </div>
