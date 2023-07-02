8097874835594b29b843e9b81607c29b976e661e
syzhao53 <syzhao@sas.upenn.edu>
2023-07-02 01:21:04 -0400

    bio edit

--- site/src/components/Home.js
+++ site/src/components/Home.js
@@ -65,9 +65,9 @@ function Home({ currPage, setCurrPage, theme, setTheme }) {
           HI, I&apos;M SYLVIA ZHAO
         </div>
         <div className="paragraph">
-          UX & Visual Designer &middot; Senior in Cognitive & Computer Science at UPenn
+          UX & Visual Designer &middot; UX Design Analyst @ JPMorgan Chase
           <br />
-          PennApps Co-Head of Creative &middot; Recent Design Ops Intern at Logitech
+          Based in DFW area &middot; UPenn '23 B.A. in Cognitive & Computer Science
         </div>
         <img id="home-illus" src={illustration} alt="landing illustration" />
         {/* <LandingLight theme={theme} setTheme={setTheme}/> */}
